4bd315913846215cc982148cd7baa872d62a1262
Yin <doyin@deloitte.com.cn>
2024-04-10 18:09:45 +0800

    initial version

---
+++ index.js
@@ -0,0 +1,9 @@
+const arrayAverage = require('./arrayAverage')
+const flattenArray = require('./flattenArray')
+const deepCopy = require('./deepCopy')
+const numberArrayToString = require('./numberArrayToString')
+const truncateString = require('./truncateString')
+
+module.exports = (num) => {
+    return num % 2 !== 0;
+  }
